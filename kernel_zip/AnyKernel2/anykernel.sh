bd8b22d096705e0411d3b6e14bdcf046c5f176b7
bhb27 <fglfgl27@gmail.com>
2016-10-10 21:29:58 -0300

    Patch anykernel2 to iSu
    Signed-off-by: bhb27 <fglfgl27@gmail.com>

--- kernel_zip/AnyKernel2/anykernel.sh
+++ kernel_zip/AnyKernel2/anykernel.sh
@@ -3,18 +3,20 @@
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=DirtyV by bsmitty83 @ xda-developers
-do.devicecheck=1
-do.initd=1
+kernel.string=iSu by bhb27 @ xda-developers
+do.devicecheck=0
+do.initd=0
 do.modules=0
-do.cleanup=1
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+do.cleanup=0
+device.name1=
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 
 # shell variables
+# if 1 install if any other clean
+install_isu=1
 block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
 
 ## end setup
@@ -222,34 +224,19 @@ chmod 644 $ramdisk/sbin/media_profiles.xml
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-append_file init.rc "run-parts" init;
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "dvbootscript" init.tuna;
-
-# init.superuser.rc
+if [ $install_isu == 1 ]; then
+# iSu patch include
 if [ -f init.superuser.rc ]; then
   backup_file init.superuser.rc;
-  replace_string init.superuser.rc "Superuser su_daemon" "# su daemon" "\n# Superuser su_daemon";
   prepend_file init.superuser.rc "SuperSU daemonsu" init.superuser;
 else
   replace_file init.superuser.rc 750 init.superuser.rc;
-  insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc";
+  insert_line init.rc "init.superuser.rc" after "import /init.environ.rc" "import /init.superuser.rc";
 fi;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "nodiratime,barrier=0" "nodev,noatime,nodiratime,barrier=0,data=writeback,noauto_da_alloc,discard";
-patch_fstab fstab.tuna /cache ext4 options "barrier=0,nomblk_io_submit" "nosuid,nodev,noatime,nodiratime,errors=panic,barrier=0,nomblk_io_submit,data=writeback,noauto_da_alloc";
-patch_fstab fstab.tuna /data ext4 options "nomblk_io_submit,data=writeback" "nosuid,nodev,noatime,errors=panic,nomblk_io_submit,data=writeback,noauto_da_alloc";
-append_file fstab.tuna "usbdisk" fstab;
+else
+# iSu patch remover
+remove_section init.superuser.rc "# isu daemon" "# isu daemon end"
+fi;
 
 # end ramdisk changes
 
