b020f4aa4b68f4bb54ba681c53b92940565b23b8
Felipe Leon <fglfgl27@gmail.com>
2016-12-26 11:09:09 -0200

    Add a cmdline patch for safety net pass
    experimental but this made me pass safety net on a CM base ROM a thing that I did not manage for some time
    values must be:
    androidboot.verifiedbootstate=green
    androidboot.flash.locked=1
    buildvariant=user
    androidboot.bl_state=0
    
    Not all devices have all of this values, nexus seems to have androidboot.verifiedbootstate, motorola androidboot.bl_state and androidboot.flash.locked
    other vendor may have others
    androidboot.selinux doesn't matter as long it is set to enforcing when trying a safety pass
    buildvariant may not really matters but doesn't hurt
    
    also fix some minor kernel zip related
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- kernel_zip/AnyKernel2/ak2-core.sh
+++ kernel_zip/AnyKernel2/ak2-core.sh
@@ -59,6 +59,13 @@ write_boot() {
     sed -ri 's/ androidboot.selinux=permissive|androidboot.selinux=enforcing|androidboot.selinux=disabled//g' boot.img-cmdline
     echo $(cat boot.img-cmdline) androidboot.selinux=permissive > boot.img-cmdline
   fi;
+  if [ $docmdline == 1 ]; then
+    sed -ri 's/ androidboot.flash.locked=[0-1]//g' boot.img-cmdline
+    sed -ri 's/ androidboot.bl_state=[0-2]//g' boot.img-cmdline
+    sed -ri 's/ androidboot.verifiedbootstate=green|androidboot.verifiedbootstate=yellow|androidboot.verifiedbootstate=orange|androidboot.verifiedbootstate=red//g' boot.img-cmdline
+    sed -ri 's/ buildvariant=eng|buildvariant=user|buildvariant=userdebug//g' boot.img-cmdline
+    echo $(cat boot.img-cmdline) buildvariant=user androidboot.verifiedbootstate=green androidboot.bl_state=0 androidboot.flash.locked=1 > boot.img-cmdline
+  fi;
   cmdline=`cat *-cmdline`;
   board=`cat *-board`;
   base=`cat *-base`;
--- kernel_zip/AnyKernel2/anykernel.sh
+++ kernel_zip/AnyKernel2/anykernel.sh
@@ -7,7 +7,7 @@ kernel.string=iSu by bhb27 @ xda-developers
 do.devicecheck=0
 do.initd=0
 do.modules=0
-do.cleanup=0
+do.cleanup=1
 device.name1=
 device.name2=
 device.name3=
@@ -20,11 +20,13 @@ is_slot_device=0;
 install_isu=1;
 #Boot in permissive function
 dopermissive=1;
+#Patch cmline
+docmdline=0;
 ## end setup
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. /tmp/anykernel/ak2-core.sh;
 
 ## AnyKernel permissions
 # set permissions for included ramdisk files
@@ -33,19 +35,20 @@ chmod -R 755 $ramdisk
 ## AnyKernel install
 dump_boot;
 
-if [ $install_isu == 1 ]; then
-	# iSu patch include
-	if [ -f init.superuser.rc ]; then
-	  replace_file init.superuser.rc 750 init.superuser.rc;
+if [ $docmdline == 0 ]; then
+	if [ $install_isu == 1 ]; then
+		# iSu patch include
+		if [ -f init.superuser.rc ]; then
+		  replace_file init.superuser.rc 750 init.superuser.rc;
+		else
+		  replace_file init.superuser.rc 750 init.superuser.rc;
+		  insert_line init.rc "init.superuser.rc" after "import /init.environ.rc" "import /init.superuser.rc";
+		fi;
 	else
-	  replace_file init.superuser.rc 750 init.superuser.rc;
-	  insert_line init.rc "init.superuser.rc" after "import /init.environ.rc" "import /init.superuser.rc";
+		# iSu patch remover
+		remove_section init.superuser.rc "# isu daemon" "# isu daemon end"
 	fi;
-else
-	# iSu patch remover
-	remove_section init.superuser.rc "# isu daemon" "# isu daemon end"
 fi;
-
 # end ramdisk changes
 
 write_boot;
