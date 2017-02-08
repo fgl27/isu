98e829d5297907d1f6fc7e0ef52e037fb2f7dd70
Felipe Leon <fglfgl27@gmail.com>
2017-02-08 17:42:01 -0200

    kernel_zip print default.prop patch
    fix else if this is not java
    
    Signed-off-by: Felipe Leon <fglfgl27@gmail.com>

--- kernel_zip/AnyKernel2/anykernel.sh
+++ kernel_zip/AnyKernel2/anykernel.sh
@@ -15,6 +15,8 @@ device.name1=
 is_slot_device=0;
 # 0 clean kernel support, 1 kernel support, 2 patch default.prop
 install_isu=1;
+# used by up-bin to print on the default.prop patch
+do.isu=0
 #Boot in permissive function
 dopermissive=1;
 #Patch cmline
@@ -38,7 +40,7 @@ if [ $docmdline == 0 ]; then
 	if [ $install_isu == 0 ]; then
 		# iSu patch remover
 		remove_section init.superuser.rc "# isu daemon" "# isu daemon end"
-	else if [ $install_isu == 1 ]; then
+	elif [ $install_isu == 1 ]; then
 		# iSu patch include
 		if [ -f init.superuser.rc ]; then
 		  replace_file init.superuser.rc 750 init.superuser.rc;
@@ -46,7 +48,7 @@ if [ $docmdline == 0 ]; then
 		  replace_file init.superuser.rc 750 init.superuser.rc;
 		  insert_line init.rc "init.superuser.rc" after "import /init.environ.rc" "import /init.superuser.rc";
 		fi;
-	else if [ $install_isu == 2 ]; then
+	elif [ $install_isu == 2 ]; then
 		# iSu patch default.prop
 		replace_line default.prop "ro.debuggable=1" "ro.debuggable=0"
 	fi;
